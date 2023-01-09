2a0ba5f34d30992cc2af7cf7879d61d36550e361
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-01-09 20:27:41 +0700

    initial commit from Create Vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Companies from './components/Companies';
+import Featured from './components/Featured';
+import Hero from './components/Hero';
+import Navbar from './components/Navbar';
+import Testimonials from './components/Testimonials';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Companies />
+      <Featured />
+      <Testimonials />
+    </>
+  );
+}
+
+export default App;
