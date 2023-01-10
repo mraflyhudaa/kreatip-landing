9b2a8d73f4655bf04149db5d56816b2c667d8836
Muhammad Rafly Huda <mraflyhudaa@gmail.com>
2023-01-10 14:35:16 +0700

    style: extend new custom margin and padding value

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,11 +7,13 @@ module.exports = {
         kreatip: '#040D21',
       },
       margin: {
+        7.5: '1.875rem' /* 30px */,
         13: '3.125rem' /* 50px */,
         15: '3.75rem' /* 60px */,
       },
       padding: {
         13: '3.125rem' /* 50px */,
+        17: '4.25rem' /* 68px */,
         19: '4.688rem' /* 75px */,
         18: '4.375rem' /* 70px */,
       },
